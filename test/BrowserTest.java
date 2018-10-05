b23ec7335d37000617b5a1f5d98b6f82d076c4b5
Elias Brunner <elias_brunner@gmx.ch>
2018-10-05 16:24:53 +0200

    change ip for tests to success

--- test/BrowserTest.java
+++ test/BrowserTest.java
@@ -23,7 +23,7 @@ public class BrowserTest extends WithBrowser {
      */
     @Test
     public void test() {
-        browser.goTo("http://localhost:" + play.api.test.Helpers.testServerPort());
+        browser.goTo("http://152.96.56.79:" + play.api.test.Helpers.testServerPort());
         assertTrue(browser.pageSource().contains("Your new application is ready."));
     }
 
