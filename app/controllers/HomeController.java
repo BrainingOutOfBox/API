1d4d4fcc8a698ef922f78e061647e0065fb4e968
Elias Brunner <elias_brunner@gmx.ch>
2018-10-12 10:46:27 +0200

    little changes

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -8,32 +8,19 @@ import play.mvc.*;
 
 import views.html.*;
 
-/**
- * This controller contains an action to handle HTTP requests
- * to the application's home page.
- */
-
 @Api(value = "/home", description = "All operations with home", produces = "application/json")
 public class HomeController extends Controller {
 
-    /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
-     */
     @ApiOperation(
-            nickname = "getAllEmployee",
-            value = "Get all employee engaged in the hospital",
-            notes = "With this method you can get all employee and all it's informations",
+            nickname = "getHome",
+            value = "Get home view",
+            notes = "With this method you can get the index view",
             httpMethod = "GET",
             response = HomeController.class)
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "OK", response = HomeController.class),
-            @ApiResponse(code = 401, message = "Unauthorized", response = HomeController.class),
             @ApiResponse(code = 500, message = "Internal Server ErrorMessage", response = HomeController.class) })
     public Result index() {
-
         return ok(index.render("Your new application is ready."));
 
     }
