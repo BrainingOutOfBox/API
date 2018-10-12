9c186fe15ffd1e75a081b73120a063987e826e77
Elias Brunner <elias_brunner@gmx.ch>
2018-10-12 15:08:14 +0200

    to joda time

--- app/controllers/TimerController.java
+++ app/controllers/TimerController.java
@@ -6,6 +6,7 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import models.ErrorMessage;
 import models.SuccessMessage;
+import org.joda.time.DateTime;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -13,10 +14,13 @@ import play.mvc.Result;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+
 @Api(value = "/time", description = "All operations with time", produces = "application/json")
 public class TimerController extends Controller {
-    private Date initDate;
-    private Date nowDate;
+
+    final long ONE_MINUTE_IN_MILLIS = 60000;
+    private DateTime finishDate;
+    private DateTime nowDate;
 
     @ApiOperation(
             nickname = "createTimer",
@@ -28,8 +32,8 @@ public class TimerController extends Controller {
             @ApiResponse(code = 200, message = "OK", response = SuccessMessage.class),
             @ApiResponse(code = 500, message = "Internal Server ErrorMessage", response = ErrorMessage.class) })
     public Result createTimer() {
-        initDate = new Date();
-        return ok(Json.toJson(new SuccessMessage("Success", initDate.toString())));
+        finishDate = new DateTime().plusMinutes(5);
+        return ok(Json.toJson(new SuccessMessage("Success", finishDate.toString())));
     }
 
     @ApiOperation(
@@ -43,16 +47,16 @@ public class TimerController extends Controller {
             @ApiResponse(code = 500, message = "Internal Server ErrorMessage", response = ErrorMessage.class) })
     public Result calculateDateDifference(){
         long difference = 0;
-        if (initDate != null) {
-            nowDate = new Date();
-            difference = getDateDiff(initDate, nowDate, TimeUnit.MILLISECONDS);
+        if (finishDate != null) {
+            nowDate = new DateTime();
+            difference = getDateDiff(finishDate, nowDate, TimeUnit.MILLISECONDS);
         }
         return ok(Json.toJson(difference));
     }
 
 
-    private static long getDateDiff(Date date1, Date date2, TimeUnit timeUnit) {
-        long diffInMillisecondes = date2.getTime() - date1.getTime();
+    private static long getDateDiff(DateTime date1, DateTime date2, TimeUnit timeUnit) {
+        long diffInMillisecondes = date1.getMillis()- date2.getMillis();
         return timeUnit.convert(diffInMillisecondes,TimeUnit.MILLISECONDS);
     }
 
