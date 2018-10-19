8da0487112aa52e783f0c3066fb5461b115593f9
Oli Dias <oli.dias94@protonmail.com>
2018-10-19 17:03:10 +0200

    Add custom sslengineprovider

---
+++ app/config/SslEngineProvider.java
@@ -0,0 +1,92 @@
+package config;
+
+import play.server.ApplicationProvider;
+import play.server.api.SSLEngineProvider;
+
+import javax.net.ssl.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.*;
+import java.security.cert.CertificateException;
+import java.util.Arrays;
+import java.util.List;
+
+public class SslEngineProvider implements SSLEngineProvider {
+    private ApplicationProvider applicationProvider;
+    private final String KEYSTORE_PATH = "/etc/letsencrypt/live/sinv-56079.edu.hsr.ch/keyStore.jks";
+    private final List<String> priorityCipherSuites = Arrays.asList(
+            "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA",
+            "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA",
+            "TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA");
+
+    public SslEngineProvider(ApplicationProvider applicationProvider) {
+        this.applicationProvider = applicationProvider;
+    }
+
+    @Override
+    public SSLEngine createSSLEngine() {
+        SSLContext context = createSSLContext();
+        SSLEngine engine = context.createSSLEngine();
+        List<String> cipherSuites = Arrays.asList(engine.getEnabledCipherSuites());
+        priorityCipherSuites.removeIf(c -> cipherSuites.contains(c));
+        engine.setEnabledCipherSuites(priorityCipherSuites.toArray(new String[0]));
+        SSLParameters params = engine.getSSLParameters();
+        params.setUseCipherSuitesOrder(true);
+        engine.setSSLParameters(params);
+        return engine;
+    }
+
+    private SSLContext createSSLContext(){
+        try{
+            KeyManager[] keyManagers = readKeyManagers();
+            SSLContext context = SSLContext.getInstance("TLS");
+            context.init(keyManagers, new TrustManager[]{},null);
+            return context;
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnrecoverableKeyException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private String readConfigPassword(){
+        return System.getProperty("play.server.https.keyStore.password");
+    }
+    private InputStream readKeyStoreInputStream() throws IOException {
+        String path = System.getProperty("play.server.https.keyStore.path");
+        if(path == null || path.isEmpty()){
+            path = KEYSTORE_PATH;
+        }
+        Path keystorePath = FileSystems.getDefault().getPath(path);
+        if(keystorePath!=null){
+            return Files.newInputStream(keystorePath);
+        }
+        throw new IOException("Keystore Path not found: "+ keystorePath.toString());
+    }
+    private KeyManager[] readKeyManagers() throws IOException, UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, CertificateException {
+        char[] password = readConfigPassword().toCharArray();
+        InputStream keystoreStream = readKeyStoreInputStream();
+        try{
+            KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
+            keystore.load(keystoreStream,password);
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+            kmf.init(keystore,password);
+            return kmf.getKeyManagers();
+        }finally{
+            keystoreStream.close();
+        }
+    }
+}
