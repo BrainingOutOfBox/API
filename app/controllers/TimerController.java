e9a358e091de2f5fd9015b9335274c7d96650ed9
Elias Brunner <elias_brunner@gmx.ch>
2018-10-25 14:40:57 +0200

    new methode to get timediff from specific finding

--- app/controllers/TimerController.java
+++ app/controllers/TimerController.java
@@ -1,25 +1,55 @@
 package controllers;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import com.mongodb.ConnectionString;
+import com.mongodb.async.SingleResultCallback;
+import com.mongodb.async.client.MongoClient;
+import com.mongodb.async.client.MongoClients;
+import com.mongodb.async.client.MongoCollection;
+import com.mongodb.async.client.MongoDatabase;
+import io.swagger.annotations.*;
+import models.BrainstormingFinding;
 import models.ErrorMessage;
 import models.SuccessMessage;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
 import org.joda.time.DateTime;
+import play.Logger;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 import java.util.Date;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
+import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Filters.eq;
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
-@Api(value = "/time", description = "All operations with time", produces = "application/json")
+
+@Api(value = "/Timing", description = "All operations with time", produces = "application/json")
 public class TimerController extends Controller {
 
     private DateTime finishDate;
     private DateTime nowDate;
+
+    private MongoClient mongoClient;
+    private MongoDatabase database;
+    CodecRegistry pojoCodecRegistry;
+    MongoCollection<BrainstormingFinding> collection;
+
+    public TimerController(){
+        pojoCodecRegistry = fromRegistries(MongoClients.getDefaultCodecRegistry(), fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+        mongoClient = MongoClients.create(new ConnectionString("mongodb://play:Methode746@localhost:40002/?authSource=admin&authMechanism=SCRAM-SHA-1"));
+
+        database = mongoClient.getDatabase("Test");
+        database = database.withCodecRegistry(pojoCodecRegistry);
+
+        collection = database.getCollection("BrainstormingFinding", BrainstormingFinding.class);
+
+    }
 
     @ApiOperation(
             nickname = "createTimer",
@@ -38,7 +68,7 @@ public class TimerController extends Controller {
     @ApiOperation(
             nickname = "calculateTimeDifference",
             value = "Calculate difference in time",
-            notes = "With this method you can calculate the time difference form now to init date",
+            notes = "With this method you can calculate the time difference from now to init date",
             httpMethod = "GET",
             response = SuccessMessage.class)
     @ApiResponses(value = {
@@ -51,6 +81,39 @@ public class TimerController extends Controller {
             difference = getDateDiff(finishDate, nowDate, TimeUnit.MILLISECONDS);
         }
         return ok(Json.toJson(difference));
+    }
+
+    @ApiOperation(
+            nickname = "calculateTimeDifference",
+            value = "Calculate difference in time",
+            notes = "With this method you can calculate the time difference of the current round in the selected BrainstormFinding",
+            httpMethod = "GET",
+            response = SuccessMessage.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK", response = SuccessMessage.class),
+            @ApiResponse(code = 500, message = "Internal Server ErrorMessage", response = ErrorMessage.class) })
+    public Result calculateDateDifferenceOfFindingFromTeam(@ApiParam(value = "BrainstormingTeam Name", name = "teamName", required = true) String teamName, @ApiParam(value = "BrainstormingFinding Name", name = "findingName", required = true) String findingName) throws ExecutionException, InterruptedException {
+
+        CompletableFuture<BrainstormingFinding> future = new CompletableFuture<>();
+        DateTime currentRoundEndTime;
+        DateTime nowDate;
+
+        collection.find(and(eq("brainstormingTeam.name", teamName),eq("name", findingName))).first(new SingleResultCallback<BrainstormingFinding>() {
+            @Override
+            public void onResult(final BrainstormingFinding brainstormFinding, final Throwable t) {
+                Logger.info(brainstormFinding.getCurrentRoundEndTime());
+                future.complete(brainstormFinding);
+            }
+        });
+
+        long difference = 0;
+        currentRoundEndTime = DateTime.parse(future.get().getCurrentRoundEndTime());
+        if (currentRoundEndTime != null) {
+            nowDate = new DateTime();
+            difference = getDateDiff(currentRoundEndTime, nowDate, TimeUnit.MILLISECONDS);
+        }
+        return ok(Json.toJson(difference));
+
     }
 
 
