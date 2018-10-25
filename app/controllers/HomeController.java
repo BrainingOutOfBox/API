75cf5bc54275b83225e21903f3f2d16987e7b96d
Elias Brunner <elias_brunner@gmx.ch>
2018-10-25 14:40:07 +0200

    some refactoring

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -8,6 +8,8 @@ import com.typesafe.config.Config;
 import io.swagger.annotations.*;
 import jwt.VerifiedJwt;
 import jwt.filter.Attrs;
+import models.ErrorMessage;
+import models.SuccessMessage;
 import play.Logger;
 import play.libs.Json;
 import play.mvc.*;
@@ -41,13 +43,21 @@ public class HomeController extends Controller {
 
     }
 
-
+    @ApiOperation(
+            nickname = "login",
+            value = "Login",
+            notes = "With this method you can login an get an jwt Token",
+            httpMethod = "GET",
+            response = Json.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK", response = Json.class),
+            @ApiResponse(code = 500, message = "Internal Server ErrorMessage", response = ErrorMessage.class) })
     public Result login() throws UnsupportedEncodingException {
         JsonNode body = request().body().asJson();
 
         if (body == null) {
             Logger.error("json body is null");
-            return forbidden();
+            return forbidden(Json.toJson(new ErrorMessage("Error", "json body is null")));
         }
 
         if (body.hasNonNull("username") && body.hasNonNull("password") && body.get("username").asText().equals("abc")) {
@@ -56,9 +66,9 @@ public class HomeController extends Controller {
             return ok(result);
         } else {
             Logger.error("json body not as expected: {}", body.toString());
+            return forbidden(Json.toJson(new ErrorMessage("Error", "json body not as expected")));
         }
 
-        return forbidden();
     }
 
     private String getSignedToken(Long userId) throws UnsupportedEncodingException {
@@ -76,8 +86,8 @@ public class HomeController extends Controller {
         Optional<VerifiedJwt> oVerifiedJwt = request().attrs().getOptional(Attrs.VERIFIED_JWT);
         return oVerifiedJwt.map(jwt -> {
             Logger.debug(jwt.toString());
-            return ok("access granted via filter");
-        }).orElse(forbidden("eh, no verified jwt found"));
+            return ok(Json.toJson(new SuccessMessage("Success", "access granted via filter")));
+        }).orElse(forbidden(Json.toJson(new ErrorMessage("Error","eh, no verified jwt found"))));
     }
 
 
