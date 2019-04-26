579960526907911dcf84acdbf850c111de3e8762
Elias Brunner <elias_brunner@gmx.ch>
2019-04-26 08:29:07 +0200

    Update Idea.java

--- app/models/bo/Idea.java
+++ app/models/bo/Idea.java
@@ -2,8 +2,6 @@ package models.bo;
 
 import net.steppschuh.markdowngenerator.MarkdownCascadable;
 import net.steppschuh.markdowngenerator.MarkdownElement;
-import net.steppschuh.markdowngenerator.MarkdownSerializationException;
-import org.bson.codecs.pojo.annotations.BsonIgnore;
 
 public abstract class Idea extends MarkdownElement implements MarkdownCascadable {
     private String description;
@@ -24,24 +22,4 @@ public abstract class Idea extends MarkdownElement implements MarkdownCascadable
         this.description = description;
     }
 
-    @BsonIgnore
-    @Override
-    public String getPredecessor() {
-        return "- ";
-    }
-
-    @BsonIgnore
-    @Override
-    public String getSuccessor() {
-        return "\n";
-    }
-
-    @BsonIgnore
-    @Override
-    public String serialize() throws MarkdownSerializationException {
-        if (getDescription() == null) {
-            throw new MarkdownSerializationException("Description is null");
-        }
-        return getPredecessor() + getDescription() + getSuccessor();
-    }
 }
